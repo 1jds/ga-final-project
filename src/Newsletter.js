39f6b5b8f083d096ef5ca2ce9a1ac823596981da
Your Name <jamesdstacey2@gmail.com>
2023-04-22 00:23:13 +1000

    Updated newsletter with submit functionality

--- src/Newsletter.js
+++ src/Newsletter.js
@@ -1,15 +1,49 @@
+import { useState } from "react";
+
 export default function Newsletter() {
+  const [responseBody, setResponseBody] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+
+  const handleFormInput = (e) => {
+    const { id, value } = e.target;
+    setResponseBody((prevState) => ({ ...prevState, [id]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Form submission without non-ok response handling
+    const requestOptions = {
+      method: "POST",
+      body: JSON.stringify(responseBody),
+      headers: { "Content-Type": "application/json" },
+    };
+    if (responseBody.firstname && responseBody.lastname && responseBody.email) {
+      fetch("https://reqres.in/api/posts", requestOptions)
+        .then((response) => response.json())
+        .then((data) => console.log("Posted Data:", data))
+        .catch((error) => console.log("Error!", error));
+    } else {
+      alert("A required field is missing. Please review the form."); // Obviously, this is ugly, and it would be better to have some kind of pop-up or tool-tip indicating which form fields are causing the problem. It would also be good to have input validations, but there's no time for that right now :|
+    }
+  };
+
   return (
     <div className="about-page--container">
       <h2>Newsletter Sign-up</h2>
       <p className="newsletter-page--tagline">
         I would like to receive your amazing, exciting imaginary newsletter!
       </p>
-      <p className="newsletter-page--asterisk-note">An asterisk (*) denotes a required field.</p>
-      <form className="newsletter-page--form-element">
+      <p className="newsletter-page--asterisk-note">
+        An asterisk (*) denotes a required field.
+      </p>
+      <form onSubmit={handleSubmit} className="newsletter-page--form-element">
         <div className="newsletter-page--form-input-element">
           <label htmlFor="firstname">First Name*</label>
           <input
+            onChange={(e) => handleFormInput(e)}
             type="text"
             id="firstname"
           />
@@ -17,19 +51,23 @@ export default function Newsletter() {
         <div className="newsletter-page--form-input-element">
           <label htmlFor="firstname">Last Name*</label>
           <input
+            onChange={(e) => handleFormInput(e)}
             type="text"
-            id="firstname"
+            id="lastname"
           />
         </div>
         <div className="newsletter-page--form-input-element">
           <label htmlFor="firstname">Email*</label>
-          <input
-            type="text"
-            id="firstname"
-          />
+          <input onChange={(e) => handleFormInput(e)} type="email" id="email" />
         </div>
       </form>
-      <button className="newsletter-page--subscribe-btn">Subscribe</button>
+      <button
+        onClick={handleSubmit}
+        type="submit"
+        className="newsletter-page--subscribe-btn"
+      >
+        Subscribe
+      </button>
       <p className="newsletter-page--form-terms">
         By clicking "Subscribe," you agree to The Poetry Foundation's collection
         and use of your personal information submitted through this form in
