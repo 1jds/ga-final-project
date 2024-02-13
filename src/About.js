53e81244837caa8bd2d64a532403f1ddbded751d
1jds <jamesdstacey2@gmail.com>
2024-02-13 11:46:39 +1100

    adding more responsiveness

--- src/About.js
+++ src/About.js
@@ -3,15 +3,17 @@ import teamMeetingImg from "./assets/vintage_business_meeting.jpg";
 export default function About() {
   return (
     <div className="about-page--container">
-      <h2 className="outlet-page--main-h2-heading">About Us</h2>
-      <img
-        className="about-page--hero-img"
-        src={teamMeetingImg}
-        alt="our company team"
-      />
-      <p className="about-page--image-caption-text">
-        Some imaginary staff from The Poetry Place's fictional history.
-      </p>
+      <div className="about-page--hero-container">
+        <h2 className="outlet-page--main-h2-heading">About Us</h2>
+        <img
+          className="about-page--hero-img"
+          src={teamMeetingImg}
+          alt="our company team"
+        />
+        <p className="about-page--image-caption-text">
+          Some imaginary staff from The Poetry Place's fictional history.
+        </p>
+      </div>
       <div className="about-page--history-info-container">
         <div>
           <p>The Story and History of The Poetry Place</p>
@@ -19,27 +21,37 @@ export default function About() {
         <div>
           <h3>Our Programs</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet,
-            consequuntur! Dignissimos dolor quae iusto porro veniam modi impedit
-            illo similique quod asperiores, praesentium quas non dolore corrupti
-            ab fugit est repellat exercitationem laudantium neque delectus
-            deleniti? Eligendi reiciendis error adipisci perferendis alias.
-            Labore dicta distinctio reiciendis ad corporis voluptas. Mollitia
-            architecto repudiandae corrupti facere esse soluta, placeat qui. Id
-            cupiditate saepe iusto voluptatibus alias debitis natus?
+            Welcome to The Poetry Place, where words weave tales and emotions
+            take flight. Our journey began in the heart of creativity, sparked
+            by a passion for the art of expression. Founded in 2020, The Poetry
+            Place was born out of a dream to create a haven for poets, writers,
+            and lovers of literature to unite.
           </p>
           <p>
-            Quos, nesciunt eius itaque minus esse sequi quod omnis commodi a
-            placeat debitis dignissimos illum explicabo magnam repellendus
-            aperiam ab pariatur. Facilis nobis similique dolores magni
-            dignissimos asperiores excepturi consequuntur, consectetur pariatur
-            laudantium reprehenderit illo iusto tempore quibusdam blanditiis,
-            voluptatum cumque earum quaerat ipsa, facere tenetur cupiditate
-            dicta vero. Officiis sint officia facilis iusto in asperiores saepe
-            sapiente labore minus sit. Rem eum quam quisquam ex aliquid dolores
-            porro laboriosam consectetur nostrum doloribus neque, dignissimos
-            voluptas cupiditate. Soluta culpa ullam consequatur libero illum
-            deserunt.
+            Our story is etched in the ink of inspiration. The founders, Jemima
+            and Big Ted, envisioned a space where verses could dance freely,
+            transcending boundaries and echoing diverse voices. It all started
+            with a simple belief: poetry has the power to transform, connect,
+            and celebrate the shared human experience.
+          </p>
+          <p>
+            As we traversed the landscape of literary exploration, The Poetry
+            Place emerged as more than just a platform – it became a community.
+            With each poem shared and every story told, our community grew,
+            fostering an environment of encouragement and support.
+          </p>
+          <p>
+            Today, The Poetry Place stands as a testament to the resilience of
+            creativity. It's a digital sanctuary where poets of all backgrounds
+            and styles converge to celebrate the written word. Our diverse
+            collection reflects the richness of human emotion, exploring themes
+            from love and loss to resilience and hope.
+          </p>
+          <p>
+            Join us in our poetic odyssey, where every line tells a story, and
+            every stanza becomes a chapter in the collective narrative of The
+            Poetry Place. Together, let's continue to shape the future of
+            literary expression and celebrate the timeless beauty of poetry.
           </p>
         </div>
       </div>
